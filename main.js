3e421da490964d5deca579fcb0fd1c6d36ec438b
K-Honsu <feranmia51@gmail.com>
2023-10-18 15:14:04 +0100

    feat: add global middleware file

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 const express = require("express")
 const config = require("./config/mongoose")
+const userRouter = require("./users/users.routers")
+const authRouter = require("./auth/auth.router")
 require("dotenv").config()
 
 const port = process.env.PORT
@@ -8,6 +10,9 @@ config.connect()
 const app = express()
 
 app.use(express.json())
+
+app.use("/auth", authRouter)
+app.use("/users", userRouter)
 
 app.get('*', (req, res) => {
     return res.status(404).json({
