da0c5c86a2a1a61b1a60dded33f19875290c0d9d
K-Honsu <feranmia51@gmail.com>
2023-10-19 11:50:16 +0100

    feat: set-up nodemailer and

--- users/users.controllers.js
+++ users/users.controllers.js
@@ -1,4 +1,5 @@
 const UserModel = require("../models/user")
+const transporter = require("../mailer")
 
 const createUser = async (req, res) => {
     try {
@@ -20,6 +21,21 @@ const createUser = async (req, res) => {
             gender,
             password
         })
+
+        const mailerOption = {
+            from : "donotreply@gmail.com",
+            to : email,
+            subject : "Welcome to Blog APP",
+            text : `Dear ${user.username}\nThank you for creating an account with us. \n We are very glad to have you here!. We do hope you enjoy our service. \n Thank you.`
+        }
+
+        transporter.sendMail(mailerOption, (error, info) => {
+            if (error) {
+                console.log("Error sending mail", error);
+            } else {
+                console.log("Email sent", info.response)
+            }
+        })
         return res.status(201).json({
             status : "success",
             message : "User created successfully",
@@ -33,6 +49,102 @@ const createUser = async (req, res) => {
     }
 }
 
+const getUser = async (req, res) => {
+    try {
+        const user_id = req.user._id
+        const user = await UserModel.findById({_id : user_id})
+        return res.status(200).json({
+            status : "success",
+            data : user
+        })
+    } catch (error) {
+        return res.status(422).json({
+            status : "error",
+            message : error.message
+        })
+    }
+}
+
+const updateUserInfo = async (req, res) => {
+    try {
+        const user_id = req.user._id
+        const {first_name, last_name, gender, username} = req.body
+        const user = await UserModel.findById({_id : user_id})
+        if (!user) {
+            return res.status(404).json({
+                status : "error",
+                data : 'User not found'
+            })
+        }
+        if (username) {
+            user.username = username
+        }
+        if (gender) {
+            user.gender = gender
+        }
+        if (last_name) {
+            user.last_name = last_name
+        }
+        if (first_name) {
+            user.first_name = first_name
+        }
+        await user.save()
+        return res.status(200).json({
+            status : "success",
+            message : "User Profile Updated Successfully",
+            user 
+        })
+    } catch (error) {
+        return res.status(422).json({
+            status : "error",
+            message : error.message
+        })
+    }
+}
+
+const deleteUser = async (req, res) => {
+    try {
+        const user_id = req.user._id
+        const user = await UserModel.findOne({_id : user_id})
+        if (!user) {
+            return res.status(404).json({
+                status : "error",
+                message : "User not found"
+            })
+        }
+        const mailerOption = {
+            from : "donotreply@gmail.com",
+            to : user.email,
+            subject : "Going so soon 🥹🥹",
+            text : `Dear ${user.username}\n We are sad to see you go. 
+            \n We hope you did enjoy our sevices. If not, we kindly ask you send us a feedback on our official twitter handle to see how we can get you back on board.
+            \n Thank you.`
+        }
+
+        transporter.sendMail(mailerOption, (error, info) => {
+            if (error) {
+                console.log("Error sending mail", error);
+            } else {
+                console.log("Email sent", info.response)
+            }
+        })
+        await user.deleteOne()
+        return res.status(200).json({
+            status : "success",
+            message : "Profile deleted successfully"
+        })
+    } catch (error) {
+        return res.status(422).json({
+            status : "error",
+            message : error.message
+        })
+    }
+}
+
+
 module.exports = {
-    createUser
+    createUser,
+    getUser,
+    updateUserInfo,
+    deleteUser
 }
