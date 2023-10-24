a871595a7827acfb5c1562edc52e9fc37c853851
K-Honsu <feranmia51@gmail.com>
2023-10-24 19:11:43 +0100

    fix: refactor login and view router

--- views/views.routers.js
+++ views/views.routers.js
@@ -1,19 +1,38 @@
 const express = require("express")
 const blogService = require("../blog/blog.services")
+const userServices = require("../auth/auth.services")
+const jwt = require("jsonwebtoken")
 const cookieParser = require("cookie-parser")
 
 const router = express.Router()
 
 router.use("/public", express.static("public"))
 
-
 router.get("/blog", async (req, res) => {
-    res.render("blog")
+    const response = await blogService.getAllBlogs()
+    const blogs = Array.isArray(response.data) ? response.data : [];
+    res.render("blog", {blogs })
 })
 
 router.get("/login", async(req, res) => {
-    res.render("login")
+    res.render("login", {message : null})
 })
+
+router.post("/log", async (req, res) => {
+    console.log(req.body)
+    const response = await userServices.Login({
+        username: req.body.username,
+        password: req.body.password,
+    });
+    if (response.code === 200) {
+        res.cookie("jwt", response.data.token, { maxAge: 360000 })
+        res.redirect("blog");
+    } else if (response.code === 422) {
+        res.render("login", {message : response.data})
+    } else {
+        res.render("login", {message : response.data})
+    }
+});
 
 router.get("/welcome", async (req, res) => {
     res.render("welcome")
